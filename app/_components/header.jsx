6c8704bf8a377cb6ef2fae699e8552183ff327e7
Joel D <joeldasari10@gmail.com>
2025-04-05 16:29:55 +0530

    First commit

---
+++ app/_components/header.jsx
@@ -0,0 +1,14 @@
+import { CirclePlay, Play, SquarePlay } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="py-8 px-16">
+      <h1 className="text-3xl font-semibold flex items-center gap-2">
+        <Play className="bg-purple-400" />
+        <span>shortvideo</span> <span className="text-purple-400">ai</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
