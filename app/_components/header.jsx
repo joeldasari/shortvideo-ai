72f00426d05ee22f36daab0c0bf8eb6a00cecc38
Joel D <joeldasari10@gmail.com>
2025-04-05 17:08:39 +0530

    Updated the favicon

--- app/_components/header.jsx
+++ app/_components/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="py-8 px-16">
       <h1 className="text-3xl font-semibold flex items-center gap-2">
-        <Play className="bg-purple-400" />
+        <Play className="bg-purple-500" />
         <span>shortvideo</span> <span className="text-purple-400">ai</span>
       </h1>
     </div>
